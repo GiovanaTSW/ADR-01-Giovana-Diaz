7a9fff053a3053f87380da269f2f5ddc03f99e62
asus <asus@LAPTOP-H6PM25FQ>
2026-07-22 14:36:58 -0600

    feat: UsuarioTests.cs

---
+++ Dressly.Tests/UsuarioTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+using Dressly.Domain.Entities;
+
+namespace Dressly.Tests
+{
+    public class UsuarioTests
+    {
+        [Fact]
+        public void InstanciarUsuario_CreaObjetoCorrectamente()
+        {
+            // Arrange
+            var usuario = new Usuario();
+
+            // Act & Assert
+            Assert.NotNull(usuario);
+        }
+    }
+}
