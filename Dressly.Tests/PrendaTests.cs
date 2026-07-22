4a296587749780a9625efd83f68067f6a6dba941
asus <asus@LAPTOP-H6PM25FQ>
2026-07-22 14:35:58 -0600

    feat: PrendaTests.cs

---
+++ Dressly.Tests/PrendaTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+using Dressly.Domain.Entities;
+
+namespace Dressly.Tests
+{
+    public class PrendaTests
+    {
+        [Fact]
+        public void InstanciarPrenda_CreaObjetoCorrectamente()
+        {
+            // Arrange
+            var prenda = new Prenda();
+
+            // Act & Assert
+            Assert.NotNull(prenda);
+        }
+    }
+}
