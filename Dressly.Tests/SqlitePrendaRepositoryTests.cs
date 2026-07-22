3d90e24c211076fc164fdd4046aaa946580a379f
asus <asus@LAPTOP-H6PM25FQ>
2026-07-22 14:38:06 -0600

    feat: SqlitePrendaRepositoryTests.cs

---
+++ Dressly.Tests/SqlitePrendaRepositoryTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+using Dressly.Infrastructure.Repositories;
+
+namespace Dressly.Tests
+{
+    public class SqlitePrendaRepositoryTests
+    {
+        [Fact]
+        public void InstanciarRepositorioPrenda_NoEsNulo()
+        {
+            // Arrange & Act
+            var repo = new SqlitePrendaRepository(null);
+
+            // Assert
+            Assert.NotNull(repo);
+        }
+    }
+}
